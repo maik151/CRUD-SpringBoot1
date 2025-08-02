4ff62ec4a75b93f27357a446da099ae0c03abf7f
maicol_jb <64624079+maik151@users.noreply.github.com>
2025-08-01 20:49:50 -0500

    Agregar Libreria Swagger para la documentacion de la API

--- src/main/java/com/proyecto1/crud/CrudApplication.java
+++ src/main/java/com/proyecto1/crud/CrudApplication.java
@@ -2,6 +2,18 @@ package com.proyecto1.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "ApiProductosClientes",
+        version = "1.0",
+        description = "ApiProductosClientes - CRUD Operations for Products and Clients"
+    )
+)
 
 @SpringBootApplication
 public class CrudApplication {
